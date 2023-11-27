8b801b178f34a670aae8529d0eada1019c0beba4
Kavisha4 <kvsha2002@gmail.com>
2023-11-27 23:10:30 +0530

    Commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Navbar,Container} from 'react-bootstrap';
 import {useState,useEffect} from "react";
+
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 export const NavBar=()=>{
    const {activeLink,setActiveLink} =useState('home');
    const {scrolled,setScrolled}=useState(false);
@@ -22,6 +27,9 @@ export const NavBar=()=>{
     window.addEventListener("scroll",onScroll);
 
 },[])
+  const onUpdateActiveLink=(value)=>{
+    setActiveLink(value);
+  }
   return (
     <Navbar expand="lg" className={scrolled? "scrolled":""}>
       <Container>
@@ -40,9 +48,9 @@ export const NavBar=()=>{
           </Nav>
           <span classnameName="navbar-text">
             <div className='social-icon'>
-                <a href="#"><img src={} alt=""/></a>
-                <a href="#"><img src={} alt=""/></a>
-                <a href="#"><img src={} alt=""/></a>
+                <a href="#"><img src={navIcon1} alt=""/></a>
+                <a href="#"><img src={navIcon2} alt=""/></a>
+                <a href="#"><img src={navIcon3} alt=""/></a>
             </div>
             <button className='vvd' onClick={()=>console.log("connect")}><span>Lets connect</span></button>
           </span>
